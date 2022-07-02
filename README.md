1c447e2711af762f317f2a5dc72aa6097e515353
IOMisaka <zekashima@qq.com>
2022-07-02 16:33:38 +0800

    initial

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # bilibilifollowtime
-显示b站关注时间的脚本
+显示b站关注时间的用户脚本
+
+# 安装
+需要
++ [Tampermonkey](https://www.tampermonkey.net/)
+
+已有Tampermonkey
+
++ 点击安装[blft.user.js](blft.user.js)
+
+# 效果图
+![img](1.webp)
+![img](2.webp)
---
+++ blft.user.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         显示b站关注时间
+// @namespace    https://github.com/IOMisaka/bilibilifollowtime
+// @version      1.0
+// @description  在空间页面显示关注时间
+// @author       IOMisaka
+// @match        https://space.bilibili.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    let div = document.createElement("div");
+    div.title = "点击关闭";
+    div.style.border = "2px solid";
+    div.style.borderColor = "grey";
+    div.style.backgroundColor = "rgb(255 38 100)";
+    div.style.color = "white";
+    div.style.borderRadius = "2px";
+    div.style.right = "0px";
+    div.style.bottom = "0px";
+    div.style.position = "fixed";
+    div.style.zIndex = 1111;
+
+    let txt = document.createElement("span");
+    txt.innerHTML = "";
+    div.appendChild(txt);
+
+    let close = document.createElement("span");
+    close.innerText = "Loading...";
+    close.onclick = ()=>{div.style.setProperty('display','none');};
+    close.style.cursor = "pointer";
+    div.appendChild(close);
+
+    document.querySelector("body").appendChild(div);
+
+    let uid = location.href.match(/com\/(\d*)[\/\?]*/)[1];
+    fetch("https://api.bilibili.com/x/space/acc/relation?mid="+uid, {"credentials": "include"}).then(res => res.json()).then(follow => {
+        if(follow.data.relation.mid === undefined || follow.data.relation.mid==0){
+            txt.parentNode.style.setProperty('display','none');
+        }else{
+            fetch("https://api.bilibili.com/x/space/acc/info?mid="+uid,{"credentials":"include"}).then(res => res.json()).then(user =>{
+                txt.innerHTML = "您关注<strong style='color:#1affff'>"+user.data.name+"</strong>的时间为："+ new Date(follow.data.relation.mtime * 1000).toLocaleString();
+                close.innerText = "❎";
+            });
+        }
+    });
+})();
